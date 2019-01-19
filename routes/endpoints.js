8d051e33918efcb94e2c46f8d21546aeae4246de
ishanisri <ishasri.1212@gmail.com>
2019-01-19 23:27:31 +0530

    Added login and register controllers

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -1,13 +1,36 @@
 exports = module.exports = {
-    clientRegistrationController
+    clientRegistrationController,
+    clientLoginController,
+    partnerLoginController,
+    partnerRegistrationController
 }
 
 
 // own modules
 const package = require("../package");
 
+function partnerRegistrationController(req, res) {
+    console.log(req.query);
+    res.status(200);
+}
+
 
 function clientRegistrationController(req, res) {
     console.log(req.query);
     res.status(200);
+}
+
+function partnerRegistrationController(req, res) {
+    console.log(req.query);
+    res.status(200);
+}
+
+function partnerLoginController(req, res) {
+    console.log(req.query);
+    res.status(200);
+}
+
+function clientLoginController(req, res) {
+  console.log(req.query);
+  res.status(200);
 }
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ const api = require("./endpoints");
 
 // other routes
 router.get("/registerclient", api.clientRegistrationController);
-
+router.get("/loginclient", api.clientLoginController);
+router.get("/loginpartner", api.partnerLoginController);
+router.get("/registerPartner", api.partnerRegistrationController);
 
 exports = module.exports = router;
