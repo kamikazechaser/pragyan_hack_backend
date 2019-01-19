3a3699912f5045aeae77d8384661bad9ea394cd5
sandy9999 <luckysandy99@gmail.com>
2019-01-20 01:09:54 +0530

    Find distances between points

--- pythagoras.js
+++ pythagoras.js
@@ -1,4 +1,21 @@
-function shortest_dist_points(x1, y1, x2, y2)
-{
-    return Math.sqrt((x2*x2 - x1*x1) + (y2*y2 - y1*y1))
-}
+function simpleDist(pointA, pointB) {
+    var x = pointA.x - pointB.x,
+        y = pointA.y - pointB.y;
+  
+    return Math.sqrt(x*x + y*y);
+  }
+  
+  var sortByDist = (function() {
+    var comparator = function(a,b) { return a.value - b.value; };
+  
+    return function (pointRef, pointArray) {
+      var reorder = function(e) { return pointArray[e.index]; };
+      var distanceFromArray = function(b,i) {
+        return { index: i, value: simpleDist(pointRef, b) };
+      };
+      return pointArray.map(distanceFromArray).sort(comparator).map(reorder);
+    };
+  }());
+
+  //console.log(simpleDist({x: 1, y: 3}, {x: 2, y: 5}))
+  //console.log(sortByDist({x: 10, y: 9}, [{x: 7, y: 4},{x: 1, y: 2}, {x: 6, y: 8}]))
